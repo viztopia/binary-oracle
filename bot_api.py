2c758749e6d91a04facfe5c279e8512b1163a2f3
Yuguang (YG) Zhang <37890050+viztopia@users.noreply.github.com>
2024-05-21 10:45:23 -0400

    update gemini to 1.5 pro latest

--- bot_api.py
+++ bot_api.py
@@ -44,7 +44,11 @@ safety_settings = [
   },
 ]
 
-model = genai.GenerativeModel(model_name="gemini-1.5-pro",
+# # print models
+# for model in genai.list_models():
+#     print(model)
+
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
